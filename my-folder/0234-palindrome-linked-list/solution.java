6267ce449b82befe83bdc53df346c5469ac7ae9e
Zaheen Siddiqui <zaheensiddiqui05@gmail.com>
2026-07-16 03:32:30 +0000

    [LeetCode Sync] Runtime - 34 ms (5.30%), Memory - 101.8 MB (5.25%)

---
+++ my-folder/0234-palindrome-linked-list/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        int n = 0;
+        ListNode end = head;
+        // ListNode
+        boolean Pal = true;
+        Stack<Integer> stack = new Stack<>();
+        while(end != null){
+            stack.push(end.val);
+            end = end.next;
+        }
+        while(head != null){
+            int top = stack.pop();
+            if(top != head.val){
+                Pal = false;
+            }
+            head = head.next;
+        }
+        return Pal;
+    }
+}
