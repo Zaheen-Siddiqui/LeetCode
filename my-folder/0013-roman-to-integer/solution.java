51fd63ae9f96cfbf98a2f466e728f6572afb04c9
Zaheen Siddiqui <zaheensiddiqui05@gmail.com>
2026-07-17 06:32:13 +0000

    [LeetCode Sync] Runtime - 4 ms (54.44%), Memory - 46.5 MB (63.02%)

---
+++ my-folder/0013-roman-to-integer/solution.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> table = new HashMap<>();
+
+        table.put('I', 1);
+        table.put('V', 5);
+        table.put('X', 10);
+        table.put('L', 50);
+        table.put('C', 100);
+        table.put('D', 500);
+        table.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = table.get(s.charAt(i));
+
+            if (i + 1 < s.length() && current < table.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+}
