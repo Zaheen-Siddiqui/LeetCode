5a0cfdf24de69cdf5abdb10a4284368006ce952e
Zaheen Siddiqui <zaheensiddiqui05@gmail.com>
2026-07-21 06:42:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (96.94%)

---
+++ my-folder/0232-implement-queue-using-stacks/solution.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> in;
+    private Stack<Integer> out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    private void move() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+    }
+
+    public int pop() {
+        move();
+        return out.pop();
+    }
+
+    public int peek() {
+        move();
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
+
