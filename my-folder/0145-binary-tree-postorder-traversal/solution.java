5f69104fe2bb22e9be21e08a1d0489bf28c287de
Zaheen Siddiqui <zaheensiddiqui05@gmail.com>
2026-07-22 04:19:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.4 MB (15.51%)

---
+++ my-folder/0145-binary-tree-postorder-traversal/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Traversal(root, list);
+
+        return list;
+    }
+
+    public void Traversal(TreeNode root, List<Integer> list){
+        if(root == null)
+            return;
+
+        Traversal(root.left, list);
+        Traversal(root.right, list);
+        list.add(root.val);
+    }
+}
